cfde7c79ea6189b90d49d74fcd4233f0b9307018
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-31 10:41:13 +0000

    main api request done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-      <h1>*happy seal noises*</h1>
+      <h1>Welcome to the PokeDex!</h1>
       <PokeDex />
       </div>
     );
--- src/containers/PokeDex.js
+++ src/containers/PokeDex.js
@@ -4,6 +4,28 @@ import SelectedView from '../components/SelectedView';
 
 class PokeDex extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      pokemon: []
+    };
+  }
+
+  componentDidMount() {
+    const url = 'https://pokeapi.co/api/v2/pokemon/?limit=10';
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+
+    request.addEventListener("load", () => {
+      if (request.status !== 200) return;
+      const jsonString = request.responseText;
+      const data = JSON.parse(jsonString);
+      this.setState({pokemon: data.results})
+    });
+
+    request.send();
+  }
+
   render(){
     return(
       <div>
